1498ebd47540456786eaa730c0711e0d7c317822
Brandon Lowe <BLoweCLE@gmail.com>
2023-04-11 10:17:37 -0500

    heroku solution

--- ormconfig.js
+++ ormconfig.js
@@ -6,31 +6,40 @@ export const dbConfig = {
     }
 };
 
-switch (process.env.NODE_ENV) {
-    case 'development':
-        Object.assign(dbConfig, {
-            type: 'sqlite',
-            database: 'db.sqlite',
-            entities: ['**/*.entity.js']
-        });
-        break;
-    case 'test':
-        Object.assign(dbConfig, {
-            type: 'sqlite',
-            database: 'test.sqlite',
-            entities: ['**/*.entity.ts']
-            });
-    case 'production':
-        Object.assign(dbConfig, {
-            type: 'postgres',
-            url: process.env.DATABASE_URL,
-            entities: ['**/*.entity.js'],
-            ssl: {
-                rejectUnauthorized: false
-            }
-        });
-    default:
-        throw new Error('unknown environment');
-}
+Object.assign(dbConfig, {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: ['**/*.entity.js'],
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+// switch (process.env.NODE_ENV) {
+//     case 'development':
+//         Object.assign(dbConfig, {
+//             type: 'sqlite',
+//             database: 'db.sqlite',
+//             entities: ['**/*.entity.js']
+//         });
+//         break;
+//     case 'test':
+//         Object.assign(dbConfig, {
+//             type: 'sqlite',
+//             database: 'test.sqlite',
+//             entities: ['**/*.entity.ts']
+//             });
+//     case 'production':
+        // Object.assign(dbConfig, {
+        //     type: 'postgres',
+        //     url: process.env.DATABASE_URL,
+        //     entities: ['**/*.entity.js'],
+        //     ssl: {
+        //         rejectUnauthorized: false
+        //     }
+        // });
+//     default:
+//         throw new Error('unknown environment');
+// }
 
 export default dbConfig;
